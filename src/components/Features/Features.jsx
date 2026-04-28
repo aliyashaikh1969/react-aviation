6b3f575f804a88d570795f2bfba3a65515d725a5
aliyashaikh1969 <aliyashaikh1969@gmail.com>
2026-04-28 20:05:37 +0530

    feature design done

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -8,41 +8,41 @@ import { IoShieldCheckmarkOutline } from "react-icons/io5";
 export const Features = () => {
   return (
     <>
-      <div>
-        <div>
-          <div>
-            <span><IoPricetagsOutline /></span>
+      <div className='flex px-36 py-6'>
+        <div className='flex items-center gap-3 flex-[20%]'>
+          <div className='bg-[#5ac3fc4c] rounded-md w-12 h-12 flex items-center justify-center'>
+            <span className='text-3xl'><IoPricetagsOutline /></span>
           </div>
           <div>
-            <p>Best Price Guarantee</p>
-            <p>We ensure you get the best fares on every booking</p>
-          </div>
-        </div>
-        <div>
-          <div>
-            <span><TfiHeadphoneAlt /></span>
-          </div>
-          <div>
-            <p>24/7 Customer Support</p>
-            <p>we're here to help you anytime, anywhere.</p>
+            <p className='font-semibold text-sm'>Best Price Guarantee</p>
+            <p className='text-xs text-gray-500 max-w-40'>We ensure you get the best fares on every booking</p>
           </div>
         </div>
-        <div>
-          <div>
-            <span> <BsCalendar2Check /></span>
+        <div className='flex items-center gap-3 flex-[20%]'>
+          <div className='bg-[#5ac3fc4c] rounded-md w-12 h-12 flex items-center justify-center'>
+            <span className='text-3xl'><TfiHeadphoneAlt /></span>
           </div>
           <div>
-            <p>Best Price Guarantee</p>
-            <p>We ensure you get the best fares on every booking</p>
+            <p className='font-semibold text-sm'>24/7 Customer Support</p>
+            <p className='text-xs text-gray-500 max-w-40'>we're here to help you anytime, anywhere.</p>
           </div>
         </div>
-         <div>
-          <div>
-            <span><IoShieldCheckmarkOutline /></span>
+        <div className='flex items-center gap-3 flex-[20%]'>
+          <div className='bg-[#5ac3fc4c] rounded-md w-12 h-12 flex items-center justify-center '>
+            <span className='text-3xl'> <BsCalendar2Check /></span>
           </div>
           <div>
-            <p>Best Price Guarantee</p>
-            <p>We ensure you get the best fares on every booking</p>
+            <p className='font-semibold text-sm'>Easy Booking</p>
+            <p className='text-xs text-gray-500 max-w-40'>Simple steps to book your perfect flight.</p>
+          </div>
+        </div>
+         <div className='flex items-center gap-3 flex-[20%]'>
+          <div className='bg-[#5ac3fc4c] rounded-md w-12 h-12 flex items-center justify-center '>
+            <span className='text-3xl'><IoShieldCheckmarkOutline /></span>
+          </div>
+          <div>
+            <p className='font-semibold text-sm'>Safe & Secure</p>
+            <p className='text-xs text-gray-500 max-w-40'>Your data is safe and protected.</p>
           </div>
         </div>
       </div>
